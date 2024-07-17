cf06687eda5aeff1c66952622b12c5e3982a5011
kaylee-k <kaylee.kihm@gmail.com>
2024-07-17 14:59:44 -0600

    Implementing AddTodo

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+export default function AddTodo({ onAdd }) {
+  const [text, setText] = useState('');
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length === 0) {
+      return;
+    }
+    onAdd({ id: 'unique value', text, status: 'active' });
+    setText('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Add Todo'
+        value={text}
+        onChange={handleChange}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import AddTodo from '../AddTodo/AddTodo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
-    { id: '123', text: '장보기', status: 'active' },
-    { id: '12', text: '공부하기', status: 'active' },
+    { id: '123', text: 'Shopping for groceries', status: 'active' },
+    { id: '12', text: 'Studying', status: 'active' },
   ]);
 
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
   return (
     <section>
       <ul>
@@ -13,6 +17,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
