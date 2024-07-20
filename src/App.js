d1b589ea652a500179d95b8f4a4356337ffead70
kaylee-k <kaylee.kihm@gmail.com>
2024-07-19 23:00:00 -0600

    Style entire app

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const filters = ['all', 'active', 'completed'];
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <>
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </div>
+    </>
   );
 }
 
